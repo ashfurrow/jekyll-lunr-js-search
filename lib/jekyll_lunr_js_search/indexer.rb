de2d46453b78bc7a4277e87d939c67a8084421f6
Ash Furrow <ash@ashfurrow.com>
2017-03-25 18:44:30 -0400

    Removed puts.

--- lib/jekyll_lunr_js_search/indexer.rb
+++ lib/jekyll_lunr_js_search/indexer.rb
@@ -74,7 +74,6 @@ module Jekyll
             "categories" => entry.categories,
             "body" => entry.body
           }
-          puts entry.url
 
           @index.add(doc)
           doc.delete("body")
